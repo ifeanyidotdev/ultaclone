d01557bcfb9458501250a12973a4bc9141248ddc
Ifeanyichukwu Sampson Ebenezer <40169444+Mremmalex@users.noreply.github.com>
2023-01-31 14:07:20 +0100

    deploy

--- backend/prisma/migrations/20230130204415_product_update/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `category` on the `Product` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Product" DROP COLUMN "category";
